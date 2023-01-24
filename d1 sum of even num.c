1cfbc65a1b8214b926fd80cf55d6416fed437aaa
harshitha0315/192111390 <112242455+harshitha0315@users.noreply.github.com>
2023-01-24 14:41:32 +0530

    Add files via upload

---
+++ d1 sum of even num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	printf("enter the limit:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d+",i);
+			sum=sum+i;
+		}
+	}
+	printf("=%d",sum);
+	return 0;
+}
